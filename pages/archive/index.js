01d6d1f415e95e04e0329065db3d30ab60d649f2
BJTiew <132333550+BJTiew@users.noreply.github.com>
2023-06-21 21:30:13 +0800

    Add files via upload

---
+++ pages/archive/index.js
@@ -0,0 +1,62 @@
+import { getGlobalNotionData } from '@/lib/notion/getNotionData'
+import React from 'react'
+import { useGlobal } from '@/lib/global'
+import BLOG from '@/blog.config'
+import { useRouter } from 'next/router'
+import { getLayoutByTheme } from '@/themes/theme'
+
+const ArchiveIndex = props => {
+  const { siteInfo } = props
+  const { locale } = useGlobal()
+
+  // 根据页面路径加载不同Layout文件
+  const Layout = getLayoutByTheme(useRouter())
+
+  const meta = {
+    title: `${locale.NAV.ARCHIVE} | ${siteInfo?.title}`,
+    description: siteInfo?.description,
+    image: siteInfo?.pageCover,
+    slug: 'archive',
+    type: 'website'
+  }
+
+  props = { ...props, meta }
+
+  return <Layout {...props} />
+}
+
+export async function getStaticProps() {
+  const props = await getGlobalNotionData({ from: 'archive-index' })
+  // 处理分页
+  props.posts = props.allPages.filter(page => page.type === 'Post' && page.status === 'Published')
+  delete props.allPages
+
+  const postsSortByDate = Object.create(props.posts)
+
+  postsSortByDate.sort((a, b) => {
+    const dateA = new Date(a?.date?.start_date || a.createdTime)
+    const dateB = new Date(b?.date?.start_date || b.createdTime)
+    return dateB - dateA
+  })
+
+  const archivePosts = {}
+
+  postsSortByDate.forEach(post => {
+    const date = post.date?.start_date?.slice(0, 7) || post.createdTime
+    if (archivePosts[date]) {
+      archivePosts[date].push(post)
+    } else {
+      archivePosts[date] = [post]
+    }
+  })
+
+  props.archivePosts = archivePosts
+  delete props.allPages
+
+  return {
+    props,
+    revalidate: parseInt(BLOG.NEXT_REVALIDATE_SECOND)
+  }
+}
+
+export default ArchiveIndex
